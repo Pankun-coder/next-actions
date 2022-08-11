fd93126f957f0017550d7c02caf4fdf40f995813
pankun-coder <jjohese@gmail.com>
2022-08-11 16:36:12 +0900

    add author page

--- pages/ssr.js
+++ pages/ssr.js
@@ -5,7 +5,7 @@ export default function Ssr(data) {
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch("rails-actions-service.my-service/users");
+  const res = await fetch("http://rails-actions-service.my-service:80/users");
   const data = await res.json();
 
   // Pass data to the page via props
